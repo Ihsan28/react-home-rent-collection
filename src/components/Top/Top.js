5eca5846e15094b27725f49741bf558114be4670
ihsan28 <ihsanmohammadalaqib@gmail.com>
2021-10-07 00:40:04 +0600

    first commit

---
+++ src/components/Top/Top.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Top = () => {
+    return (
+        <div>
+            <p>
+                This is a rent colection system
+            </p>
+        </div>
+    );
+};
+
+export default Top;
